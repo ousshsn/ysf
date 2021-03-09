a808940f78798dd4d9a15d79d80a899d9bfd0457
oussema hasnaoui <oussema.hasnaoui@esprit.tn>
2021-03-09 02:44:00 +0100

    first commit

---
+++ src/routes.js
@@ -0,0 +1,53 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.9.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import DashboardPage from "views/Dashboard/Dashboard.js";
+import TableList from "views/TableList/TableList.js";
+import Typography from "views/Typography/Typography.js";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin"
+  },
+
+  {
+    path: "/table",
+    name: "Analyse",
+    rtlName: "قائمة الجدول",
+    icon: "content_paste",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/typography",
+    name: "Rapport",
+    rtlName: "طباعة",
+    icon: LibraryBooks,
+    component: Typography,
+    layout: "/admin"
+  }
+];
+
+export default dashboardRoutes;
